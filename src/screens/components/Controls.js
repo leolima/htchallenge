c14a20a789e80191e6e1a6a2de4744fee7367703
Leonardo Lima <leolima.str@gmail.com>
2023-02-27 22:30:17 -0300

    feat: creating the controls component

--- src/screens/components/Controls.js
+++ src/screens/components/Controls.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext, useState, useEffect, useCallback} from 'react';
 import {
   Text,
   View,
@@ -6,17 +6,64 @@ import {
   TextInput,
   TouchableOpacity,
 } from 'react-native';
+import {FirebaseContext} from '../../contexts/FirebaseContext';
+import {isValidColor} from '../../utils/common';
 
 export const Controls = () => {
+  const [colors, setColors] = useState(undefined);
+  const {userData, saveUserData} = useContext(FirebaseContext);
+
+  const handleCuboColor = v => setColors({...colors, cubo: v});
+  const handleConeColor = v => setColors({...colors, cone: v});
+  const handleDodecaColor = v => setColors({...colors, dodecaedro: v});
+
+  const applyColors = useCallback(colors => {
+    if (
+      isValidColor(colors?.cubo) &&
+      isValidColor(colors?.cone) &&
+      isValidColor(colors?.dodecaedro)
+    ) {
+      saveUserData(colors);
+    } else {
+      alert('There is an invalid Color');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (userData) {
+      const obj = {
+        cubo: userData?.cubo || 'red',
+        cone: userData?.cone || 'yellow',
+        dodecaedro: userData?.dodecaedro || 'green',
+      };
+      setColors(obj);
+    }
+  }, [userData]);
+
   return (
     <View style={styles.container}>
       <View style={styles.rowContainer}>
-        <TextInput style={styles.input} placeholder="Cor do Cubo" />
-        <TextInput style={styles.input} placeholder="Cor do Cone" />
-        <TextInput style={styles.input} placeholder="Cor do Dodecaedro" />
+        <TextInput
+          style={styles.input}
+          placeholder="Cor do Cubo"
+          value={colors?.cubo}
+          onChangeText={handleCuboColor}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Cor do Cone"
+          value={colors?.cone}
+          onChangeText={handleConeColor}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Cor do Dodecaedro"
+          value={colors?.dodecaedro}
+          onChangeText={handleDodecaColor}
+        />
       </View>
       <View>
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={() => applyColors(colors)}>
           <View style={styles.button}>
             <Text style={styles.buttonLabel}>Aplicar</Text>
           </View>
