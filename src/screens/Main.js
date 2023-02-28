c4ff52f48004bcd225d69081a1b4cd00485e55bb
Leonardo Lima <leolima.str@gmail.com>
2023-02-27 22:30:39 -0300

    refactor: main screen

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
+import {StyleSheet, ScrollView} from 'react-native';
 import {
   Scene,
   Mesh,
@@ -9,79 +9,102 @@ import {
   DodecahedronGeometry,
   BasicShadowMap,
   BoxGeometry,
+  Color,
 } from 'three';
 import {Renderer} from 'expo-three';
 import {GLView} from 'expo-gl';
 import {Controls} from './components/Controls';
+import {FirebaseContext} from '../contexts/FirebaseContext';
 
 const App = () => {
-  const onContextCreate = async gl => {
-    const scene = new Scene();
-    const camera = new PerspectiveCamera(
-      75,
-      gl.drawingBufferWidth / gl.drawingBufferHeight,
-      0.1,
-      1000,
-    );
-    gl.canvas = {
-      width: gl.drawingBufferWidth,
-      height: gl.drawingBufferHeight,
-    };
+  const [shapes, setShapes] = useState({});
+  const {userData} = useContext(FirebaseContext);
 
-    camera.position.z = 7;
+  const onContextCreate = useCallback(
+    async gl => {
+      const scene = new Scene();
+      const camera = new PerspectiveCamera(
+        75,
+        gl.drawingBufferWidth / gl.drawingBufferHeight,
+        0.1,
+        1000,
+      );
+      gl.canvas = {
+        width: gl.drawingBufferWidth,
+        height: gl.drawingBufferHeight,
+      };
 
-    const renderer = new Renderer({gl});
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = BasicShadowMap;
-    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+      camera.position.z = 7;
 
-    // --- Materials --
-    const geometry = new BoxGeometry(1.4, 1.4, 1.4);
-    const material = new MeshBasicMaterial({color: 'red'});
-    const cube = new Mesh(geometry, material);
-    cube.position.y = 3;
+      const renderer = new Renderer({gl});
+      renderer.shadowMap.enabled = true;
+      renderer.shadowMap.type = BasicShadowMap;
+      renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-    const coneGeo = new ConeGeometry(1, 2);
-    const coneMaterial = new MeshBasicMaterial({color: 'blue'});
-    const cone = new Mesh(coneGeo, coneMaterial);
-    cone.position.y = 0;
+      // --- Materials --
+      const geometry = new BoxGeometry(1.4, 1.4, 1.4);
+      const material = new MeshBasicMaterial({color: 'red'});
+      const cube = new Mesh(geometry, material);
+      cube.position.y = 3;
 
-    const dodecagonoGeo = new DodecahedronGeometry(1, 0);
-    const dodecagonoMaterial = new MeshBasicMaterial({color: 'green'});
-    const dodecagono = new Mesh(dodecagonoGeo, dodecagonoMaterial);
-    dodecagono.position.y = -3;
+      const coneGeo = new ConeGeometry(1, 2);
+      const coneMaterial = new MeshBasicMaterial({color: 'blue'});
+      const cone = new Mesh(coneGeo, coneMaterial);
+      cone.position.y = 0;
 
-    scene.add(cube);
-    scene.add(cone);
-    scene.add(dodecagono);
+      const dodecaedroGeo = new DodecahedronGeometry(1, 0);
+      const dodecaedroMaterial = new MeshBasicMaterial({color: 'green'});
+      const dodecaedro = new Mesh(dodecaedroGeo, dodecaedroMaterial);
+      dodecaedro.position.y = -3;
 
-    const render = () => {
-      requestAnimationFrame(render);
+      setShapes({cube, cone, dodecaedro});
+      scene.add(cube);
+      scene.add(cone);
+      scene.add(dodecaedro);
 
-      cube.rotation.x += 0.001;
-      cube.rotation.y += 0.01;
+      const render = () => {
+        requestAnimationFrame(render);
 
-      cone.rotation.x += 0.01;
-      cone.rotation.y += 0.01;
+        cube.rotation.x += 0.001;
+        cube.rotation.y += 0.01;
 
-      dodecagono.rotation.x += 0.01;
-      dodecagono.rotation.y += 0.01;
+        cone.rotation.x += 0.01;
+        cone.rotation.y += 0.01;
 
-      renderer.render(scene, camera);
-      gl.endFrameEXP();
-    };
-    render();
-  };
+        dodecaedro.rotation.x += 0.01;
+        dodecaedro.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+        gl.endFrameEXP();
+      };
+      render();
+    },
+    [userData],
+  );
+
+  useEffect(() => {
+    if (userData && shapes?.cube && shapes?.cone && shapes?.dodecaedro) {
+      shapes.cube.material.color = new Color(userData?.cubo || 'red');
+      shapes.cone.material.color = new Color(userData?.cone || 'yellow');
+      shapes.dodecaedro.material.color = new Color(
+        userData?.dodecaedro || 'green',
+      );
+    }
+  }, [userData]);
 
   return (
-    <View style={styles.flex}>
+    <ScrollView style={styles.flex}>
       <GLView
         onContextCreate={onContextCreate}
-        // set height and width of GLView
-        style={{width: 400, height: 600, borderColor: 'black', borderWidth: 1}}
+        style={{
+          width: 400,
+          height: 600,
+          borderColor: 'black',
+          borderWidth: 1,
+        }}
       />
       <Controls />
-    </View>
+    </ScrollView>
   );
 };
 
@@ -90,7 +113,6 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-between',
     backgroundColor: 'black',
   },
 });
